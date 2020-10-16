f21084d7fb991bd2b508f7dcd5249763bc245a00
moonrailgun <moonrailgun@gmail.com>
2020-10-16 14:29:05 +0800

    skip no support file transform

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const pkg = require('./package.json');
 module.exports = function (snowpackConfig, pluginOptions) {
   return {
     name: pkg.name,
-    transform({ id, contents }) {
+    transform({ id, contents, fileExt }) {
+      if(fileExt !== '.js') {
+        // Skip not support version
+        return contents;
+      }
+
       const list = pluginOptions.list || [];
       for (let item of list) {
         let str = item.from;
