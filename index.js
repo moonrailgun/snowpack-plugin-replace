73c22db06e7c302c2d4515a77e87fbe6f9ca357d
Jhomas <thomas.o.morrison@gmail.com>
2020-12-22 16:06:33 -0500

    Add .html to the list of valid extensions

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
+const fs = require('fs');
 const pkg = require('./package.json');
+const glob = require('glob');
+const path = require('path');
 
-const supportExt = ['.js', '.css']
+function _findFiles({ buildDirectory, extension }) {
+  const pattern = `${buildDirectory}/**/*.${extension}`;
+  return glob.sync(pattern).map((filePath) => path.relative(buildDirectory, filePath));
+}
 
 module.exports = function (snowpackConfig, pluginOptions) {
   return {
     name: pkg.name,
-    transform({ id, contents, fileExt }) {
-      if(!supportExt.includes(fileExt)) {
-        // Skip not support version
-        return contents;
+    optimize({ buildDirectory }) {
+      const files = [];
+      if (pluginOptions.file) {
+        files.push(pluginOptions.file);
+      } else {
+        const filesFromExtension = pluginOptions.extensions.map(extension => _findFiles({ buildDirectory, extension }));
+        files.push(...filesFromExtension.flat());
       }
 
-      const list = pluginOptions.list || [];
-      for (let item of list) {
-        const file = item.file;
-        if(typeof file === 'string') {
-          // If specify file path. check it
-          if(file !== id) {
+      for (const file of files) {
+        let fileString = fs.readFileSync(path.join(buildDirectory, file), 'utf8');
+        const list = pluginOptions.list || [];
+        for (let item of list) {
+          const { from } = item;
+          if(from === undefined || from === null) {
             continue;
           }
+
+          const regularExp = new RegExp(from, 'g');
+          fileString = fileString.replace(regularExp, item.to);
         }
 
-        let str = item.from;
-        if(str === undefined || str === null) {
-          continue;
-        }
-
-        const replaceValue = item.to;
-        contents = contents.replace(str, replaceValue);
+        fs.writeFileSync(path.join(buildDirectory, file), fileString);
       }
-      return contents;
     },
   };
 };
